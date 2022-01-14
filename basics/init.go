4fad5d3b5899bd3d3c86ba65db4c94ee61931e1b
roshan <mailme.roshanbhattarai@gmail.com>
2022-01-14 09:55:34 +0545

    logs

---
+++ basics/init.go
@@ -0,0 +1,24 @@
+package main
+
+/*
+	init functions are run before main funtions.
+	you are allowed to write multiple init function anywhere in the program
+*/
+
+import "fmt"
+
+func init() {
+	fmt.Println("hello from main init")
+}
+
+func init() {
+	fmt.Println("this is first init function")
+}
+
+func main() {
+	fmt.Println("main function started after init functions")
+}
+
+func init() {
+	fmt.Println("this is second init function")
+}
